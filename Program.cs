f6f4dadcc285f057fa503ff60a21620063f8698d
Moazz <moazzemh222@gmail.com>
2022-12-17 23:35:59 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_and_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 20, 14, 27, 100, 90 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
